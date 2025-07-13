b291f604400d78dc41c4e6a1cb2d404d60df838a
shapSan <51676358+shapSan@users.noreply.github.com>
2025-07-13 15:16:43 -0700

    Update chatWithOpenAI.js

--- api/chatWithOpenAI.js
+++ api/chatWithOpenAI.js
@@ -134,6 +134,7 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
 
   if (req.method === "OPTIONS") {
     return res.status(200).end(); // respond early for preflight
