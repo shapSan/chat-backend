bff9cd110871328b7266af3fda5721e3244487a3
shapSan <51676358+shapSan@users.noreply.github.com>
2025-08-31 17:50:17 -0700

    Update viewSlide.js

--- api/viewSlide.js
+++ api/viewSlide.js
@@ -1,6 +1,14 @@
 import { get } from '@vercel/blob';
 
 export default async function handler(req, res) {
+  // Enable CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
   const { token } = req.query;
   
   if (!token) {
